cd0b4a42c92416f2636f7cd4a458de459c781ff4
Cnawel <facun85@gmail.com>
2023-01-20 15:49:52 -0300

    terminados ambos script, falta agregar opciones

--- getPictures.py
+++ getPictures.py
@@ -22,21 +22,21 @@ if response.ok:
  
     print(string_url, "\n \n downloading..........")
 
-while True:
-    filename='pic'+str(random.randint(1, 100000))+'.jpg'
-    file_exists = os.path.isfile(filename)
- 
-    if not file_exists:
-        with open(filename, 'wb+') as handle:
-            response = requests.get(string_url, stream=True)
-            if not response.ok:
-                print(response)
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-                handle.write(block)
-    else:
-        continue
-    break
-print("\n                downloading completed ..............")
+    while True:
+        filename='pic'+str(random.randint(1, 100000))+'.jpg'
+        file_exists = os.path.isfile(filename)
+    
+        if not file_exists:
+            with open(filename, 'wb+') as handle:
+                response = requests.get(string_url, stream=True)
+                if not response.ok:
+                    print(response)
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+                    handle.write(block)
+        else:
+            continue
+        break
+    print("\n                downloading completed ..............")
 
--- getPictures2.py
+++ getPictures2.py
@@ -12,7 +12,7 @@
 from instaloader import Instaloader, Profile
 
 L = Instaloader()
-PROFILE = "cervezacamus"
+PROFILE = ""
 profile = Profile.from_username(L.context, PROFILE)
 
 posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda post: post.likes,reverse=True)
